dca77819b5bfd1231aa76bdf97831615f6e6ca05
shridutt <shridutt270@gmail.com>
2021-01-08 00:21:32 +0530

    map is added in popup

--- src/Shared/UIElements/Modal/Modal.js
+++ src/Shared/UIElements/Modal/Modal.js
@@ -1,6 +1,6 @@
-import React, { useState, forwardRef } from "react";
+import React, { forwardRef } from "react";
 import {
-    Button,
+  Button,
   Dialog,
   DialogActions,
   DialogContent,
@@ -8,23 +8,47 @@ import {
   Slide,
 } from "@material-ui/core";
 
+import Map from '../Map/Map';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyle = makeStyles( (theme) => ({
+  root : {
+    '& .MuiDialog-paperWidthSm' : {
+      maxWidth : '70vw',
+    },
+    '& .MuiDialogTitle-root' : {
+      backgroundColor : theme.palette.primary.main,
+      color : 'white'
+    },
+    '& .MuiDialogContent-root' : {
+      padding : 0
+    }
+  },
+  actions : {
+    padding : '10px'
+  }
+}))
+
 const Transition = forwardRef((props, ref) => {
     return <Slide direction="down" ref={ref} {...props} />;
   });
 
 const Modal = (props) => {
-
+  const classes = useStyle();
   let { open, onCloseModal, title } = props;
   return (
       <Dialog 
       open={open}
       TransitionComponent={Transition}
       onClose={onCloseModal}
+      className={classes.root}
       >
           <DialogTitle>{title}</DialogTitle>
-          <DialogContent>Here we will render the map, TODAY</DialogContent>
-          <DialogActions>
-              <Button onClick={onCloseModal}>CLOSE</Button>
+          <DialogContent>
+            <Map center={props.center} zoom={props.zoom}/>
+          </DialogContent>
+          <DialogActions className={classes.actions}>
+              <Button variant="contained" color="primary" onClick={onCloseModal}>CLOSE</Button>
           </DialogActions>
       </Dialog>
   );
