d81c475e8174a6d6eec5a520ea6fcda50ace53dd
Shridutt Patel <shridutt270@gmail.com>
2021-01-07 20:18:05 +0530

    added dummu location modal.

---
+++ src/Shared/UIElements/Modal/Modal.js
@@ -0,0 +1,32 @@
+import React, { useState, forwardRef } from "react";
+import {
+    Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Slide,
+} from "@material-ui/core";
+
+const Transition = forwardRef((props, ref) => {
+    return <Slide direction="down" ref={ref} {...props} />;
+  });
+
+const Modal = (props) => {
+
+  let { open, onCloseModal, title } = props;
+  return (
+      <Dialog 
+      open={open}
+      TransitionComponent={Transition}
+      onClose={onCloseModal}
+      >
+          <DialogTitle>{title}</DialogTitle>
+          <DialogContent>Here we will render the map, TODAY</DialogContent>
+          <DialogActions>
+              <Button onClick={onCloseModal}></Button>
+          </DialogActions>
+      </Dialog>
+  );
+};
+export default Modal;
