d073561f6ce370e46a29c151395f50e872197c8c
Shridutt Patel <shridutt270@gmail.com>
2021-01-11 22:55:34 +0530

    input error solved

--- src/User/Pages/UserAuth.js
+++ src/User/Pages/UserAuth.js
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
-import { Typography, Container, Card, Button, Divider } from "@material-ui/core";
+import {
+  Typography,
+  Container,
+  Card,
+  Button,
+  Divider,
+} from "@material-ui/core";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import { makeStyles } from "@material-ui/styles";
 
 import TextInput from "../../Shared/UIElements/Input/TextInput";
 
-const initialValues = {
-  email: "",
-  password: "",
-  name : "",
-  cnfPassword : ""
-
-};
+let initialValues ={
+    name : "",
+    email : "",
+    password : "",
+    cnfPassword : ""
+}
 
 const useStyles = makeStyles({
   root: {
@@ -38,18 +43,20 @@ const useStyles = makeStyles({
 const UserAuth = (props) => {
   const classes = useStyles();
   const [passwordVisibility, setPasswordVisibility] = useState("password");
-  const [cnfPasswordVisibility, setCnfPasswordVisibility] = useState("password");
-  const [mode, setMode] = useState('login');
+  const [cnfPasswordVisibility, setCnfPasswordVisibility] = useState(
+    "password"
+  );
+  const [mode, setMode] = useState("login");
 
   const switchModeHandler = () => {
-      setMode(preState => {
-          if (mode === 'login') {
-              return 'signup';
-          } else {
-              return 'login';
-          }
-      })
-  }
+    setMode((preState) => {
+      if (mode === "login") {
+        return "signup";
+      } else {
+        return "login";
+      }
+    });
+  };
   const passwordVisibilityHandler = () => {
     setPasswordVisibility((preState) => setVisibility(preState));
   };
@@ -60,97 +67,112 @@ const UserAuth = (props) => {
 
   const setVisibility = (type) => {
     if (type === "text") {
-        return "password";
-      } else {
-        return "text";
-      }
-  }
+      return "password";
+    } else {
+      return "text";
+    }
+  };
 
-  let form = null
+  let form = null;
+  let validation = null;
 
-  if (mode === 'login') {
-      form = (
-        <React.Fragment>
-            <TextInput type="email" name="email" label="Email" />
-            <TextInput
-            type={passwordVisibility}
-            name="password"
-            visibilityicon="true"
-            visiblilitytoggler={passwordVisibilityHandler}
-            label="Password"
-            />
-        </React.Fragment>
-      );
+  if (mode === "login") {
+    form = (
+      <React.Fragment>
+        <TextInput type="email" name="email" label="Email" />
+        <TextInput
+          type={passwordVisibility}
+          name="password"
+          visibilityicon="true"
+          visiblilitytoggler={passwordVisibilityHandler}
+          label="Password"
+        />
+      </React.Fragment>
+    );
+
+    validation = Yup.object({
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(5, "min 5 characters or more"),
+    });
   } else {
-      form = (
-          <React.Fragment>
-                <TextInput type="text" name="name" label="Your Name" />
-                <TextInput type="email" name="email" label="Email" />
-                <TextInput
-                type={passwordVisibility || "password"}
-                name="password"
-                visibilityicon="true"
-                visiblilitytoggler={passwordVisibilityHandler}
-                label="Password"
-                />
-                <TextInput 
-                    type={cnfPasswordVisibility || "password"}
-                    name="cnfPassword"
-                    visibilityicon="true"
-                    visiblilitytoggler={cnfPasswordVisibilityHandler}
-                    label="Password"
-                />
-          </React.Fragment>
-      );
+    validation = Yup.object({
+      email: Yup.string().email().required(),
+      name: Yup.string().required().min(5, "min 5 characters or more"),
+      password: Yup.string().required().min(5, "min 5 characters or more"),
+      cnfPassword: Yup.string()
+        .required()
+        .oneOf([Yup.ref("password"), null], "Should match with above field"),
+    });
+    form = (
+      <React.Fragment>
+        <TextInput type="text" name="name" label="Your Name" />
+        <TextInput type="email" name="email" label="Email" />
+        <TextInput
+          type={passwordVisibility}
+          name="password"
+          visibilityicon="true"
+          visiblilitytoggler={passwordVisibilityHandler}
+          label="Password"
+        />
+        <TextInput
+          type={cnfPasswordVisibility || "password"}
+          name="cnfPassword"
+          visibilityicon="true"
+          visiblilitytoggler={cnfPasswordVisibilityHandler}
+          label="Password"
+        />
+      </React.Fragment>
+    );
   }
 
   return (
     <React.Fragment>
       <Formik
         initialValues={initialValues}
-        validationSchema={Yup.object({
-          email: Yup.string().email().required(),
-          name: Yup.string().required().min(5, 'min 5 characters or more'),
-          password: Yup.string().required().min(5, "min 5 characters or more"),
-          cnfPassword : Yup.string().required().oneOf([Yup.ref('password'), null], "Should match with above field")
-        })}
+        validationSchema={validation}
         onSubmit={(value, { setSubmitting, resetForm }) => {
-            setSubmitting(false);
-            resetForm(false);
-            console.log(value);
-          }}
+          setSubmitting(false);
+          resetForm(false);
+          console.log(value);
+        }}
       >
         {(props) => {
           return (
             <Container className={classes.root} maxWidth="md">
               <Card className={classes.formContainer}>
                 <Typography component="h1" variant="h4">
-                  { mode === 'login' ? "Login" : "Signup" }
+                  {mode === "login" ? "Login" : "Signup"}
                 </Typography>
-                <Divider style={{margin : '1rem 0'}}/>
+                <Divider style={{ margin: "1rem 0" }} />
                 <form onSubmit={props.handleSubmit}>
                   {form}
-                  <div style={{display: 'flex', flexDirection : 'column', alignItems : 'center'}}>
-                  <Button
-                    type="submit"
-                    size="large"
-                    variant="contained"
-                    disabled={!props.isValid || props.isSubmitting}
-                    color="primary"
-                    className={classes.sybmitBtn}
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                    }}
                   >
-                    { mode === 'login' ? "Login" : "Signup" }
-                  </Button>
-                  <Button
-                    type="button"
-                    size="large"
-                    color="primary"
-                    variant="outlined"
-                    style={{marginTop : '1rem'}}
-                    onClick={switchModeHandler}
-                  > 
-                    Switch mode to { mode === 'login' ? "Signup" : "Login" }
-                  </Button>
+                    <Button
+                      type="submit"
+                      size="large"
+                      variant="contained"
+                      disabled={!props.isValid || props.isSubmitting}
+                      color="primary"
+                      className={classes.sybmitBtn}
+                    >
+                      {mode === "login" ? "Login" : "Signup"}
+                    </Button>
+                    <Button
+                      type="button"
+                      size="large"
+                      color="primary"
+                      variant="outlined"
+                      style={{ marginTop: "1rem" }}
+                      onClick={switchModeHandler}
+                    >
+                      Switch mode to {mode === "login" ? "Signup" : "Login"}
+                    </Button>
                   </div>
                 </form>
               </Card>
