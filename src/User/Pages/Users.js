d377d7fb7c6acbafb95a164b617a8a1f9e0a0df2
shridutt <shridutt270@gmail.com>
2021-03-04 14:39:29 +0530

    getting all users done from api.

--- src/User/Pages/Users.js
+++ src/User/Pages/Users.js
@@ -1,8 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Typography, Button, CircularProgress } from '@material-ui/core';
+
 import UsersList from "../Components/UsersList";
+import ErrorModal from '../../Shared/UIElements/ErrorModal/ErrorModal';
 
 const Users = (props) => {
-  const USERS = [
+  const [USERS, setUSERS] = useState([]);
+  const [isLoading, setIsLoding] = useState(false);
+  const [error, setError] = useState();
+  const [errorModalVisibility, setErrorModalVisibility] = useState(false);
+
+  const closeErrorModal = () => {
+    setErrorModalVisibility(false);
+  }
+  useEffect(() => {
+    const getAllUsers = async () => {
+      setIsLoding(true);
+      setError(null);
+      try {
+        const response = await fetch('http://localhost:5000/api/users');
+        const responseData = await response.json();
+        if(!response.ok) {
+          throw new Error(responseData.message)
+        }
+
+        setUSERS(responseData.users);
+      } catch (e) {
+        setError(e.message || "somthing went wrong.")
+      }
+      setIsLoding(false)
+    }
+    getAllUsers();
+  }, []);
+  
+  return (
+     <React.Fragment>
+      <ErrorModal
+        open={errorModalVisibility}
+        onCloseModal={closeErrorModal}
+        title='ERROR!!'
+        actions={
+          <React.Fragment>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={closeErrorModal}
+            >
+              OK
+            </Button>
+          </React.Fragment>
+        }
+      >
+      <Typography
+          variant="h5"
+          component="h2"
+          style={{ margin: "1rem 24px", minWidth: "20rem" }}
+        >
+          {error || "Somthing went wrong. Plase try again."}
+        </Typography>
+      </ErrorModal>
+      { !isLoading ? <UsersList items={USERS} /> : <CircularProgress size={30}/>} 
+    </React.Fragment>
+  
+  )};
+export default Users;
+
+/* USERS = [
     {
       id: "u1",
       name: "Shridutt",
@@ -51,7 +114,4 @@ const Users = (props) => {
       places: 4,
       image : 'blue_person.jpeg'
     },
-  ];
-  return <UsersList items={USERS} />;
-};
-export default Users;
+  ]; */
